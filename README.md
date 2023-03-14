19ee7980b6ff63c3c9670cd7514a5439a251119f
andyholes <andyholesdev@gmail.com>
2023-03-13 22:09:32 -0300

    add query authorById

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ First time building a GraphQL API with Spring Boot
 
 Some test cases:<br>
 <img src="https://user-images.githubusercontent.com/94710776/224863983-73a5e988-5f03-46c5-bfb3-89561a875535.png">
+![img.png](img.png)
--- src/main/java/com/andyholes/graphqldemo/controller/AuthorController.java
+++ src/main/java/com/andyholes/graphqldemo/controller/AuthorController.java
@@ -3,8 +3,11 @@ package com.andyholes.graphqldemo.controller;
 import com.andyholes.graphqldemo.model.Author;
 import com.andyholes.graphqldemo.repository.AuthorRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
+
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
@@ -14,5 +17,10 @@ public class AuthorController {
     @QueryMapping //if method name differs from the one defined in the schema i can use (name="authors")
     Iterable<Author> authors(){
         return authorRepository.findAll();
+    }
+
+    @QueryMapping
+    Optional<Author> authorById(@Argument Long id){
+        return authorRepository.findById(id);
     }
 }
